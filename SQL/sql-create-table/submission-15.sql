4853600984011907b92e4b2715a02776ae3e1e7a
vinnie-winnie <vincent.h.lam00@gmail.com>
2026-09-03 17:18:41 -0700

    Add: sql-create-table - submission-15

---
+++ SQL/sql-create-table/submission-15.sql
@@ -0,0 +1,12 @@
+create table videos (id integer, name text, created_at DATE, published boolean);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
